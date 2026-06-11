d958061ab6a9f9939e339257fba2a327746528da
HuyHuy-NewBie <huyvlntb01261@gmail.com>
2026-06-11 22:58:01 +0700

    FixBug Push

--- DATN_KimTonGold-main (3)/DATN_KimTonGold-main/Web/GoldManagementSystem/Models/Product/ProductCatalogModeOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace GoldManagementSystem.Models
-{
-    public static class ProductCatalogModeOptions
-    {
-        public const string Single = "Single";
-        public const string Multi = "Multi";
-    }
-}
