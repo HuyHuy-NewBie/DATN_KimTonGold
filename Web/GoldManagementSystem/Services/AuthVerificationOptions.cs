3bea9ffe1f1b7599904ea5ef2ebba672fc19a964
chuyendoita2804 <cuongpctb01562@gmail.com>
2026-07-15 21:20:31 +0700

    removeweb

--- Web/GoldManagementSystem/Services/AuthVerificationOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace GoldManagementSystem.Services
-{
-    public class AuthVerificationOptions
-    {
-        public bool RequireLoginVerification { get; set; } = true;
-        public bool RequireRegistrationVerification { get; set; } = true;
-        public bool RequireProfileContactVerification { get; set; } = true;
-    }
-}
